0526bc7347385fdd5e2069d053bf4aec60e564b8
Alexander Mahdybor <thezaksid@gmail.com>
2016-11-06 22:30:46 +0200

    Lesson 7. Property injection
    Added dependency injection via properties
    Used <map> and <list>

--- src/main/java/com/yet/spring/core/App.java
+++ src/main/java/com/yet/spring/core/App.java
@@ -1,29 +1,54 @@
 package com.yet.spring.core;
 
 import com.yet.spring.core.beans.Client;
-import com.yet.spring.core.loggers.ConsoleEventLogger;
+import com.yet.spring.core.beans.Event;
+import com.yet.spring.core.beans.EventType;
+import com.yet.spring.core.loggers.EventLogger;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.util.Map;
+
 public class App {
     private Client client;
-    private ConsoleEventLogger eventLogger;
+    private EventLogger defaultLogger;
+    private Map<EventType, EventLogger> loggers;
 
-    public App(Client client, ConsoleEventLogger eventLogger) {
+    public App(Client client, EventLogger eventLogger, Map<EventType, EventLogger> loggers) {
         super();
         this.client = client;
-        this.eventLogger = eventLogger;
+        this.defaultLogger = eventLogger;
+        this.loggers = loggers;
     }
 
     public static void main(String[] args) {
         ConfigurableApplicationContext ctx = new ClassPathXmlApplicationContext("beans.xml");
         App app = (App) ctx.getBean("app");
 
+        Client client = ctx.getBean(Client.class);
+        System.out.println("Client says: " + client.getGreeting());
+
+        Event event = ctx.getBean(Event.class);
+        app.logEvent(EventType.INFO, event, "Some event for 1");
+
+        event = ctx.getBean(Event.class);
+        app.logEvent(EventType.ERROR, event, "Some event for 2");
+
+        event = ctx.getBean(Event.class);
+        app.logEvent(null, event, "Some event for 3");
+
         ctx.close();
     }
 
-    private void logEvent(String msg) {
+    private void logEvent(EventType eventType, Event event, String msg) {
         String message = msg.replaceAll(client.getId(), client.getFullName());
-        //eventLogger.logEvent(message);
+        event.setMsg(message);
+
+        EventLogger logger = loggers.get(eventType);
+        if (logger == null) {
+            logger = defaultLogger;
+        }
+
+        logger.logEvent(event);
     }
 }
--- src/main/java/com/yet/spring/core/beans/Client.java
+++ src/main/java/com/yet/spring/core/beans/Client.java
@@ -3,6 +3,7 @@ package com.yet.spring.core.beans;
 public class Client {
     private String id;
     private String fullName;
+    private String greeting;
 
     public Client(String id, String fullName) {
         this.id = id;
@@ -26,5 +27,13 @@ public class Client {
 
     public void setFullName(String fullName) {
         this.fullName = fullName;
+    }
+
+    public String getGreeting() {
+        return greeting;
+    }
+
+    public void setGreeting(String greeting) {
+        this.greeting = greeting;
     }
 }
